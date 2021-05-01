58e6284cf410755783d3e0bfe51edf5646627af2
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-05-01 19:00:00 -0300

    Minimal edit on Css
    css edit to make the button larger

--- php-files/file-01.php
+++ php-files/file-01.php
@@ -2,24 +2,23 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div id="formulary">
-        <form action="file-01.php" method="post">
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
         Name: <input type="text" name="name" class="form-1"><br>
         City: <input type="text" name="city" class="form-1"><br>
         Color: <input type="text" name="color" class="form-1"><br>
-        <button type="submit">Submit</button>
+        <button class="btn" type="submit">Enviar</button>
         </form>
         <?php
-        $name = $_POST["name"];
-        $city = $_POST["city"];
-        $color = $_POST["color"];
-        echo 'Hi!, ' . $name . '. I seen your city is ' . $city . ' and your favorite color is ' . $color;
+            $name = $_POST['name'];
+            $city = $_POST['city'];
+            $color = $_POST['color'];
+            echo 'Hi!, ' . $name . '. I seen your city is ' . $city . ' and your favorite color is ' . $color;
         ?>
     </div>
     
